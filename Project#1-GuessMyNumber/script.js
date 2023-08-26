2e27e308433948d0e4c5564441c2f00a52aea411
Elena Cristescu <elenacristescu11@gmail.com>
2023-08-26 17:58:19 +0200

    selecting and manipulating events

--- Project#1-GuessMyNumber/script.js
+++ Project#1-GuessMyNumber/script.js
@@ -1,1 +1,5 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = "Correct Number"
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
