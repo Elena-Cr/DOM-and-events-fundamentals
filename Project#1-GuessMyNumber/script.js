f3c26b34c0fe6416278ed5329a8e7e825d3f25c3
Elena Cristescu <elenacristescu11@gmail.com>
2023-08-28 16:25:21 +0300

    create the click event for the "Again" button

--- Project#1-GuessMyNumber/script.js
+++ Project#1-GuessMyNumber/script.js
@@ -7,10 +7,10 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 */
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
 
-document.querySelector('.number').textContent=secretNumber;
+
 
 document.querySelector(".check").addEventListener('click',function(){
   const guess = Number( document.querySelector('.guess').value);
@@ -24,6 +24,7 @@ document.querySelector(".check").addEventListener('click',function(){
   }else if (guess === secretNumber){
     document.querySelector('.message').textContent = '🎉 Correct Number'
 
+    document.querySelector('.number').textContent=secretNumber;
     document.querySelector('body').style.backgroundColor='#60b347';
     document.querySelector('.number').style.width='30rem';
 
@@ -55,4 +56,16 @@ document.querySelector(".check").addEventListener('click',function(){
       score = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener('click',function(){
+  score=20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random()*20)+1;
+  document.querySelector('.number').textContent="?";
+  document.querySelector('.message').textContent = 'Start guessing ...'
+  document.querySelector('body').style.backgroundColor='#222';
+  document.querySelector('.number').style.width='15rem';
+  document.querySelector('.guess').value='';
+  
 });
