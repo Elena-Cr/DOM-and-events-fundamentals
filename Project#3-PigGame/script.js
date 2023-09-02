25e2f533549ffcf93b86bb4bcd73bbff20c5fefd
Elena Cristescu <elenacristescu11@gmail.com>
2023-09-02 18:08:58 +0300

    add reset game functionality

--- Project#3-PigGame/script.js
+++ Project#3-PigGame/script.js
@@ -75,3 +75,17 @@ btnHold.addEventListener('click', function(){
     }
 }
 });
+
+btnNew.addEventListener('click',function(){
+//Reset scores
+score0El.textContent=0;
+score1El.textContent=0;
+current0El.textContent=0;
+current1El.textContent=0
+//Reset classes
+player0El.classList.remove('player--winner');
+player0El.classList.add('player--active');
+player1El.classList.remove('player--active');
+player1El.classList.remove('player--winner');
+diceEl.classList.add('hidden');
+});
